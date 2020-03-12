dbbd05b35dcc3ae5aef94ff257fbc3d0d07e5593
Simon <simon.mattei@heig-vd.ch>
2020-03-12 10:18:56 +0100

    splash screen launches but never goes

--- app/src/main/java/com/heig/atmanager/MainActivity.java
+++ app/src/main/java/com/heig/atmanager/MainActivity.java
@@ -2,6 +2,7 @@ package com.heig.atmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startActivity(new Intent(MainActivity.this,SplashScreenActivity.class));
+        // close splash activity
+        finish();
     }
 }
---
+++ app/src/main/java/com/heig/atmanager/SplashScreenActivity.java
@@ -0,0 +1,16 @@
+package com.heig.atmanager;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen);
+
+    }
+
+}
