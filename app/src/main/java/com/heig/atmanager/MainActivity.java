78fb21b9dcf08fc764b53dd0d93d395aa506c213
Simon <simon.mattei@heig-vd.ch>
2020-03-15 09:36:24 +0100

    splash working

--- app/src/main/java/com/heig/atmanager/MainActivity.java
+++ app/src/main/java/com/heig/atmanager/MainActivity.java
@@ -2,7 +2,6 @@ package com.heig.atmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,9 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        startActivity(new Intent(MainActivity.this,SplashScreenActivity.class));
-        // close splash activity
-        finish();
     }
+
 }
--- app/src/main/java/com/heig/atmanager/SplashScreenActivity.java
+++ app/src/main/java/com/heig/atmanager/SplashScreenActivity.java
@@ -3,14 +3,27 @@ package com.heig.atmanager;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreenActivity extends Activity {
+
+    Handler handler;
+    int splashScreenTime = 2000; //milliseconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splashscreen);
 
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },splashScreenTime);
     }
 
 }
