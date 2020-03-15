002047cf5782c052e7400195f0d220e5a8d47d65
Stephane Bottin <stephane.bottin@heig-vd.ch>
2020-03-15 16:04:05 +0100

    Calendar Fragment added

---
+++ app/src/main/java/com/heig/atmanager/CalendarFragment.java
@@ -0,0 +1,27 @@
+package com.heig.atmanager;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+/**
+ * Author : Stephane
+ * Date   : 15.03.2020
+ * <p>
+ * This class better be good.
+ */
+public class CalendarFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_calendar, container, false);
+
+        return v;
+    }
+}
