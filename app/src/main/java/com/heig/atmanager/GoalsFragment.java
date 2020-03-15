b4ff7b016c36e8952743860b7e6a9f10f2677de3
Stephane Bottin <stephane.bottin@heig-vd.ch>
2020-03-15 16:18:35 +0100

    Goals Fragment added (w/ XML)

---
+++ app/src/main/java/com/heig/atmanager/GoalsFragment.java
@@ -0,0 +1,28 @@
+package com.heig.atmanager;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+/**
+ * Author : Stéphane Bottin
+ * Date   : 15.03.2020
+ */
+public class GoalsFragment extends Fragment {
+
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_goals, container, false);
+
+        return v;
+    }
+
+}
