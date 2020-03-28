9fbed5d79c9d9cd403cadb5f89f68ab79064c849
Stephane Bottin <stephane.bottin@heig-vd.ch>
2020-03-28 17:02:07 +0100

    [WIP] color density

--- app/src/main/java/com/heig/atmanager/CalendarDayNotification.java
+++ app/src/main/java/com/heig/atmanager/CalendarDayNotification.java
@@ -1,5 +1,6 @@
 package com.heig.atmanager;
 
+import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -22,8 +23,10 @@ public class CalendarDayNotification implements LineBackgroundSpan {
     private static final String TAG = "CalendarDayNotification";
 
     private static final int HEIGHT = 50;
-    private static final int PADDING = 5;
+    private static final int PADDING = 10;
     private static final int CORNER_RADIUS = 20;
+
+    private static final int[] alphaDensity = new int[] {0, 50, 100, 150, 230};
 
     private int totalTodosAndGoals;
     private int maxTodosAndGoals;
@@ -44,10 +47,13 @@ public class CalendarDayNotification implements LineBackgroundSpan {
             int start, int end, int lineNum
     ) {
         int oldColor = paint.getColor();
+        Random rand = new Random(); // random for now
         paint.setColor(color);
+        paint.setAlpha(getDensityAlpha(rand.nextInt(maxTodosAndGoals)));
+
         // rem : charSequence = day of the month
         // user.getTodosAndGoalsForDay(Integer.parseInt(charSequence))
-        paint.setAlpha((255 * Integer.parseInt(charSequence.toString())) / maxTodosAndGoals);
+        //paint.setAlpha((255 * Integer.parseInt(charSequence.toString())) / maxTodosAndGoals);
 
         canvas.drawRoundRect(left + PADDING, top - HEIGHT + PADDING,
                 right - PADDING, bottom + HEIGHT - PADDING, CORNER_RADIUS,
@@ -56,6 +62,18 @@ public class CalendarDayNotification implements LineBackgroundSpan {
         // Bold and old color font for days
         paint.setColor(oldColor);
         paint.setTypeface(Typeface.DEFAULT_BOLD);
+    }
+
+    private int getDensityAlpha(int totalTodosAndGoals) {
+        int densityColor = 0;
+
+        for(int i = 0; i < alphaDensity.length; ++i) {
+            if (totalTodosAndGoals > i * (maxTodosAndGoals / alphaDensity.length)) {
+                densityColor = alphaDensity[i];
+            }
+        }
+
+        return densityColor;
     }
 
 }
--- app/src/main/java/com/heig/atmanager/CalendarDayToday.java
+++ app/src/main/java/com/heig/atmanager/CalendarDayToday.java
@@ -21,6 +21,10 @@ public class CalendarDayToday  implements LineBackgroundSpan {
     private static int VERTICAL_OFFSET    = 30;
     private static int HORIZONTAL_PADDING = 5;
 
+    private static final int HEIGHT = 50;
+    private static final int PADDING = 10;
+    private static final int CORNER_RADIUS = 20;
+
     int color;
 
     public CalendarDayToday(int color) {
@@ -37,12 +41,18 @@ public class CalendarDayToday  implements LineBackgroundSpan {
         paint.setColor(color);
 
         // Draw line
-        canvas.drawRect(left + HORIZONTAL_PADDING,
+        /*canvas.drawRect(left + HORIZONTAL_PADDING,
                 bottom + VERTICAL_OFFSET,
                 right - HORIZONTAL_PADDING,
-                bottom + VERTICAL_OFFSET + LINE_HEIGHT, paint);
+                bottom + VERTICAL_OFFSET + LINE_HEIGHT, paint);*/
+        paint.setStrokeWidth(8);
+        paint.setStyle(Paint.Style.STROKE);
+        canvas.drawRoundRect(left + PADDING, top - HEIGHT + PADDING,
+                right - PADDING, bottom + HEIGHT - PADDING, CORNER_RADIUS,
+                CORNER_RADIUS, paint);
 
         // Set color to white for the text that will be drawn next
         paint.setColor(Color.BLACK);
+        paint.setStyle(Paint.Style.FILL);
     }
 }
