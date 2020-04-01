effb1e0105b993b537ac573a1fe040e9aee9e695
teo <ferraricorneloup.teo@gmail.com>
2020-04-01 20:10:45 +0200

    Changed code to use android toolbar

--- app/src/main/java/com/heig/atmanager/MainActivity.java
+++ app/src/main/java/com/heig/atmanager/MainActivity.java
@@ -1,17 +1,18 @@
 package com.heig.atmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     View fragmentContainer;
-    View button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,20 +25,42 @@ public class MainActivity extends AppCompatActivity {
         //Intent myIntent = new Intent(MainActivity.this, ProfileActivity.class);
         //MainActivity.this.startActivity(myIntent);
         fragmentContainer = findViewById(R.id.folder_fragment_container);
-        button = findViewById(R.id.drawer_button);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+    }
 
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+    // Menu icons are inflated just as they were with actionbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.top_navigation_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+
+            case R.id.action_folders:
                 if(fragmentContainer.getVisibility() == View.GONE){
                     fragmentContainer.setVisibility(View.VISIBLE);
                     fragmentContainer.bringToFront();
                 } else{
                     fragmentContainer.setVisibility(View.GONE);
                 }
-            }
-        });
+                return true;
 
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
     }
+
 
     private void loadFragment(Fragment fragment) {
 
