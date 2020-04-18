5a5f94919089ac8dae55ea3c6ab79fde55a97f79
Simon <simon.mattei@heig-vd.ch>
2020-04-18 09:46:18 +0200

    working on #23 & #7

--- app/src/main/java/com/heig/atmanager/stats/StatsFragment.java
+++ app/src/main/java/com/heig/atmanager/stats/StatsFragment.java
@@ -9,7 +9,15 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Pie;
 import com.heig.atmanager.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Author : Stéphane Bottin, Mattei Simon
@@ -22,6 +30,21 @@ public class StatsFragment  extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_stats, container, false);
 
+        AnyChartView anyChartView = (AnyChartView) v.findViewById(R.id.any_chart_view);
+
+        Pie pie = AnyChart.pie();
+
+        List<DataEntry> data = new ArrayList<>();
+        data.add(new ValueDataEntry("John", 10000));
+        data.add(new ValueDataEntry("Jake", 12000));
+        data.add(new ValueDataEntry("Peter", 18000));
+
+        pie.data(data);
+
+        anyChartView.setChart(pie);
+
         return v;
     }
+
+
 }
