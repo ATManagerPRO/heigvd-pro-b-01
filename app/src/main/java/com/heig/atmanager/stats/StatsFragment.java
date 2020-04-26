20623f736d3f27dada6cca59911ddc0237084968
Simon <simon.mattei@heig-vd.ch>
2020-04-26 15:58:31 +0200

    updating charts finally works damn it

--- app/src/main/java/com/heig/atmanager/stats/StatsFragment.java
+++ app/src/main/java/com/heig/atmanager/stats/StatsFragment.java
@@ -47,9 +47,13 @@ public class StatsFragment  extends Fragment {
     private Spinner menu;
     private static final String[] items = new String[]{"Today","This Week","This Month","This Year"};
 
-    private AnyChartView pieChartTasks;
-    private AnyChartView lineChartTasks;
-    private AnyChartView pieChartGoals;
+    private AnyChartView pieChartTasksView;
+    private AnyChartView lineChartTasksView;
+    private AnyChartView pieChartGoalsView;
+
+    private Cartesian lineChartTasks;
+    private Pie pieChartTasks;
+    private Pie pieChartGoals;
 
     private static String bgColor;
 
@@ -84,22 +88,30 @@ public class StatsFragment  extends Fragment {
             }
         });
 
-
         //Charts
         user = DummyData.getUser();
 
         //TODO : get this by values.colors, not working for some reason
         bgColor = "#F1F1F1";
 
-        pieChartTasks = v.findViewById(R.id.pie_chart_tasks);
-        pieChartTasks.setBackgroundColor(bgColor);
-        lineChartTasks = v.findViewById(R.id.line_chart_tasks);
-        lineChartTasks.setProgressBar(v.findViewById(R.id.progress_bar));
-        lineChartTasks.setBackgroundColor(bgColor);
-        pieChartGoals = v.findViewById(R.id.pie_chart_goals);
-        pieChartGoals.setBackgroundColor(bgColor);
+        pieChartTasksView = v.findViewById(R.id.pie_chart_tasks);
+        APIlib.getInstance().setActiveAnyChartView(pieChartTasksView);
+        pieChartTasks = AnyChart.pie();
+        pieChartTasksView.setBackgroundColor(bgColor);
+        pieChartTasksView.setChart(pieChartTasks);
 
-        makeCharts(Interval.DAY);
+        lineChartTasksView = v.findViewById(R.id.line_chart_tasks);
+        APIlib.getInstance().setActiveAnyChartView(lineChartTasksView);
+        lineChartTasks = AnyChart.line();
+        lineChartTasksView.setProgressBar(v.findViewById(R.id.progress_bar));
+        lineChartTasksView.setBackgroundColor(bgColor);
+        lineChartTasksView.setChart(lineChartTasks);
+
+        pieChartGoalsView = v.findViewById(R.id.pie_chart_goals);
+        APIlib.getInstance().setActiveAnyChartView(pieChartGoalsView);
+        pieChartGoals = AnyChart.pie();
+        pieChartGoalsView.setBackgroundColor(bgColor);
+        pieChartGoalsView.setChart(pieChartGoals);
 
         return v;
     }
@@ -111,21 +123,19 @@ public class StatsFragment  extends Fragment {
         goals = user.getGoals().getValue();
 
         if(tasks != null) { //TODO : Something with no tasks
-            APIlib.getInstance().setActiveAnyChartView(pieChartTasks);
             makePieChartTasks();
-            APIlib.getInstance().setActiveAnyChartView(lineChartTasks);
             makeLineChartTasks(interval);
         }
 
         if(goals != null) { //TODO : Something with no goals
-            APIlib.getInstance().setActiveAnyChartView(pieChartGoals);
             makePieChartGoals();
         }
     }
 
     private void makePieChartTasks(){
 
-        Pie pie = AnyChart.pie();
+        APIlib.getInstance().setActiveAnyChartView(pieChartTasksView);
+
         List<DataEntry> data = new ArrayList<>();
         int tasksDone = 0, tasksToDo = 0;
 
@@ -137,37 +147,35 @@ public class StatsFragment  extends Fragment {
         }
 
         //since all tasks are not done, im adding 1 just to see colors TODO: remove 1
-        data.add(new ValueDataEntry("Done", tasksDone + 1));
+        data.add(new ValueDataEntry("Done", tasksDone));
         data.add(new ValueDataEntry("Todo", tasksToDo));
 
-        pie.palette(new String[]{"#3F58FF","#FF2E2E"}); //colors
-        pie.data(data); //data
-        pie.title(Task.class.getSimpleName()+ "s"); //title
-        pie.background().fill(bgColor);
-
-        pieChartTasks.setChart(pie);
+        pieChartTasks.palette(new String[]{"#3F58FF","#FF2E2E"}); //colors
+        pieChartTasks.data(data); //data
+        pieChartTasks.title(Task.class.getSimpleName()+ "s"); //title
+        pieChartTasks.background().fill(bgColor);
     }
 
     private void makeLineChartTasks(Interval interval){
 
-        Cartesian cartesian = AnyChart.line();
+        APIlib.getInstance().setActiveAnyChartView(lineChartTasksView);
 
-        cartesian.animation(true);
+        lineChartTasks.animation(true);
 
-        cartesian.padding(10d, 20d, 5d, 20d);
+        lineChartTasks.padding(10d, 20d, 5d, 20d);
 
-        cartesian.crosshair().enabled(true);
-        cartesian.crosshair()
+        lineChartTasks.crosshair().enabled(true);
+        lineChartTasks.crosshair()
                 .yLabel(true)
                 .yStroke((Stroke) null, null, null, (String) null, (String) null);
 
-        cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
+        lineChartTasks.tooltip().positionMode(TooltipPositionMode.POINT);
 
-        cartesian.title("Tasks done last " + interval.toString().toLowerCase());
+        lineChartTasks.title("Tasks done the past " + interval.name());
 
         //TODO : set title and stuff accordingly to interval
-        cartesian.xAxis(0).title("Hours/Days/Months");
-        cartesian.xAxis(0).labels().padding(5d, 5d, 5d, 5d);
+        lineChartTasks.xAxis(0).title("Hours");
+        lineChartTasks.xAxis(0).labels().padding(5d, 5d, 5d, 5d);
 
         List<DataEntry> seriesData = new ArrayList<>();
         seriesData.add(new ValueDataEntry(1,1));
@@ -178,7 +186,7 @@ public class StatsFragment  extends Fragment {
         set.data(seriesData);
         Mapping series1Mapping = set.mapAs("{ x: 'x', value: 'value' }");
 
-        Line series1 = cartesian.line(series1Mapping);
+        Line series1 = lineChartTasks.line(series1Mapping);
         series1.name("Brandy");
         series1.hovered().markers().enabled(true);
         series1.hovered().markers()
@@ -190,18 +198,16 @@ public class StatsFragment  extends Fragment {
                 .offsetX(5d)
                 .offsetY(5d);
 
-        cartesian.legend().enabled(false);
-        cartesian.legend().fontSize(13d);
-        cartesian.legend().padding(0d, 0d, 10d, 0d);
-        cartesian.background().fill(bgColor);
-
-        lineChartTasks.setChart(cartesian);
-        lineChartTasks.invalidate();
+        lineChartTasks.legend().enabled(false);
+        lineChartTasks.legend().fontSize(13d);
+        lineChartTasks.legend().padding(0d, 0d, 10d, 0d);
+        lineChartTasks.background().fill(bgColor);
     }
 
     private void makePieChartGoals(){
 
-        Pie pie = AnyChart.pie();
+        APIlib.getInstance().setActiveAnyChartView(pieChartGoalsView);
+
         List<DataEntry> data = new ArrayList<>();
         int tasksDone = 0, tasksToDo = 0;
 
@@ -213,15 +219,13 @@ public class StatsFragment  extends Fragment {
                 ++tasksToDo;
         }
 
-        data.add(new ValueDataEntry("Done", tasksDone + 1));
+        data.add(new ValueDataEntry("Done", tasksDone));
         data.add(new ValueDataEntry("Todo", tasksToDo));
 
-        pie.palette(new String[]{"#80EB5A","#FF9745"}); //colors
-        pie.data(data); //data
-        pie.title(Goal.class.getSimpleName()+ "s"); //title
-        pie.background().fill(bgColor);
-
-        pieChartGoals.setChart(pie);
+        pieChartGoals.palette(new String[]{"#80EB5A","#FF9745"}); //colors
+        pieChartGoals.data(data); //data
+        pieChartGoals.title(Goal.class.getSimpleName()+ "s"); //title
+        pieChartGoals.background().fill(bgColor);
     }
 
 
