dcaa1949b0f9d0acd216c34ed9c014f2e8224b79
Stephane Bottin <stephane.bottin@heig-vd.ch>
2020-05-05 12:08:36 +0200

    Goal fix

--- app/src/main/java/com/heig/atmanager/User.java
+++ app/src/main/java/com/heig/atmanager/User.java
@@ -44,7 +44,7 @@ public class User {
         Date dueDateGoal3 = calendar.getTime();
         calendar.add(Calendar.MONTH, 3);
         Date dueDateGoal4 = calendar.getTime();
-        Goal daily_goal1 = new Goal("SQUATS", 20, 1, Interval.DAY, dueDateGoal1);
+        Goal daily_goal1 = new Goal("SQUATS For a very looong time", 20, 1, Interval.DAY, dueDateGoal1);
         Goal daily_goal2 = new Goal("FRUITS", 5, 1, Interval.DAY, dueDateGoal2);
         Goal weekly_goal3 = new Goal("KMS", 4, 1, Interval.WEEK, dueDateGoal3);
         Goal monthly_goal4 = new Goal("GIT PUSH", 4, 1, Interval.MONTH, dueDateGoal4);
