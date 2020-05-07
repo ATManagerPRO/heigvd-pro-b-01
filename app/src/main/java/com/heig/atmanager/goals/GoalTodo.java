ae2e4f11bb8a8276715cc540673b874112aa5647
Stephane Bottin <stephane.bottin@heig-vd.ch>
2020-05-07 09:32:48 +0200

    fix merge

--- app/src/main/java/com/heig/atmanager/goals/GoalTodo.java
+++ app/src/main/java/com/heig/atmanager/goals/GoalTodo.java
@@ -42,7 +42,7 @@ public class GoalTodo {
     }
 
     public int getPercentage() {
-        int percentage = (int) (((float) quantityDone / goal.getQuantity()) * 100);
+        int percentage = (int) (((float) quantityDone / MainActivity.getUser().getGoal(goal_id).getQuantity()) * 100);
         return Math.min(percentage, 100);
     }
 
