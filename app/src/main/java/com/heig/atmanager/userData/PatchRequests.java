c28107409b335b61a4c5c96a6f0c631b9f45ba77
teo <ferraricorneloup.teo@gmail.com>
2020-05-20 11:26:17 +0200

    added patch requests for favorite and goalTodo due date

--- app/src/main/java/com/heig/atmanager/userData/PatchRequests.java
+++ app/src/main/java/com/heig/atmanager/userData/PatchRequests.java
@@ -9,6 +9,7 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.heig.atmanager.MainActivity;
+import com.heig.atmanager.goals.GoalTodo;
 import com.heig.atmanager.tasks.Task;
 
 import org.json.JSONException;
@@ -29,6 +30,78 @@ public class PatchRequests {
             jsonBody.put("todo_id", newTask.getId());
             if(newTask.getDoneDate() != null) {
                 jsonBody.put("done", sdf.format(newTask.getDoneDate()));
+            }else{
+                jsonBody.put("done", null);
+            }
+
+            JsonObjectRequest jsonObject = new JsonObjectRequest(Request.Method.PATCH, URL, jsonBody, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                }
+            }) {
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    final Map<String, String> headers = new HashMap<>();
+                    headers.put("Authorization", "Bearer " + MainActivity.getUser().getBackEndToken());//put your token here
+                    return headers;
+                }
+            };
+            Volley.newRequestQueue(context).add(jsonObject);
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static public void patchTaskFavorite(Task newTask, Context context) {
+        //post request to the server
+        try {
+            String URL = "https://atmanager.gollgot.app/api/v1/todos/"+ newTask.getId() +"/favorite";
+            JSONObject jsonBody = new JSONObject();
+
+            jsonBody.put("todo_id", newTask.getId());
+            jsonBody.put("favorite", newTask.isFavorite());
+
+            JsonObjectRequest jsonObject = new JsonObjectRequest(Request.Method.PATCH, URL, jsonBody, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                }
+            }) {
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    final Map<String, String> headers = new HashMap<>();
+                    headers.put("Authorization", "Bearer " + MainActivity.getUser().getBackEndToken());//put your token here
+                    return headers;
+                }
+            };
+            Volley.newRequestQueue(context).add(jsonObject);
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static public void patchGoalTodoDoneDate(GoalTodo newGoalTodo, Context context) {
+        //post request to the server
+        try {
+            String URL = "https://atmanager.gollgot.app/api/v1/todos/"+ newGoalTodo.getGoalTodoId() +"/done";
+            JSONObject jsonBody = new JSONObject();
+
+            android.icu.text.SimpleDateFormat sdf  = new android.icu.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+            jsonBody.put("todo_id", newGoalTodo.getGoalTodoId());
+            if(newGoalTodo.getDoneDate() != null) {
+                jsonBody.put("done", sdf.format(newGoalTodo.getDoneDate()));
             }else{
                 jsonBody.put("done", null);
             }
