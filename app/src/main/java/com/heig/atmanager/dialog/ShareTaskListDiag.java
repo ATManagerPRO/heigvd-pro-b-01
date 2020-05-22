b0c70bb28243984cde82174658d3c8bb43ab16ee
Juri <Chau-ying-kot@hotmail.com>
2020-05-22 14:06:29 +0200

    new version of dialog

--- app/src/main/java/com/heig/atmanager/dialog/ShareTaskListDiag.java
+++ app/src/main/java/com/heig/atmanager/dialog/ShareTaskListDiag.java
@@ -4,12 +4,16 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
 import com.heig.atmanager.R;
+import com.heig.atmanager.userData.PostRequests;
 
 /**
  * Share list Dialog
@@ -26,31 +30,26 @@ public class ShareTaskListDiag extends DialogFragment {
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
 
-        final boolean[] isEditable = {false};
-        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(getActivity());
-        alertBuilder.setTitle(R.string.share_taskList)
-                // Add a multichoice, if the list is editable for the receiver
-                .setMultiChoiceItems(R.array.share_option, null, new DialogInterface.OnMultiChoiceClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
-                if (isChecked)
-                   isEditable[which] = true;
-                else
-                    isEditable[which] = false;
+        AlertDialog.Builder  builder = new AlertDialog.Builder(getActivity());
 
-            }
-        }).setPositiveButton(R.string.confirm, new DialogInterface.OnClickListener() {
+        final LayoutInflater inflater = requireActivity().getLayoutInflater();
+        final View view = inflater.inflate(R.layout.fragment_shared, null);
+
+        builder.setView(view).setPositiveButton(getString(R.string.confirm), new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
 
+                final EditText inviteeEmail = view.findViewById(R.id.invitee_email);
+
+                PostRequests.postShared(taskListId, inviteeEmail.getText().toString().trim(), getContext());
             }
-        }).setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+        }).setNegativeButton(getString(R.string.cancel), new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-
+                ShareTaskListDiag.this.getDialog().cancel();
             }
         });
 
-        return alertBuilder.create();
+        return builder.create();
     }
 }
