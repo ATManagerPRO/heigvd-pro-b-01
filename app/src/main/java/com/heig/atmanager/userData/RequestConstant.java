f896e11f829f0bcc7673bf95805a7992db0e8ec1
Juri <Chau-ying-kot@hotmail.com>
2020-05-22 20:07:22 +0200

    Refactor PostResquest class to use Constants and add a missing field in postGoal

--- app/src/main/java/com/heig/atmanager/userData/RequestConstant.java
+++ app/src/main/java/com/heig/atmanager/userData/RequestConstant.java
@@ -2,28 +2,28 @@ package com.heig.atmanager.userData;
 
 public final class RequestConstant {
 
-    // /!\ All URL ends with a slash
-    private final static String BASE_URL = "https://atmanager.gollgot.app/api/v1/";
+    // /!\ All URL begins with a slash
+    private final static String BASE_URL = "https://atmanager.gollgot.app/api/v1";
 
-    public final static String DONE_EXTENSION = "done/";
-    public final static String FAVORITE_EXTENSION = "favorite/";
-    private final static String FOLDERS_EXTENSION = "folders/";
-    private final static String TODOLISTS_EXTENSION = "todolists/";
-    private final static String GOALS_EXTENSION = "goals/";
-    private final static String TODOS_EXTENSION = "todos/";
-    private final static String TAGS_EXTENSION = "tags/";
-    private final static String TODAY_EXTENSION = "today/";
-    private final static String GOAL_TODOS = "goaltodos/";
+    public final static String DONE_EXTENSION = "/done";
+    public final static String FAVORITE_EXTENSION = "/favorite";
+    private final static String FOLDERS_EXTENSION = "/folders";
+    private final static String TODOLISTS_EXTENSION = "/todolists";
+    private final static String GOALS_EXTENSION = "/goals";
+    private final static String TODOS_EXTENSION = "/todos";
+    private final static String TAGS_EXTENSION = "/tags";
+    private final static String TODAY_EXTENSION = "/today";
+    private final static String GOAL_TODOS = "/goaltodos";
 
     // Data keywords
     // - Folders
     public static final String FOLDERS_KEY       = "folders";
-    public static final String FOLDERS_ID        = "id";
-    public static final String FOLDERS_TITLE     = "label";
+    public static final String FOLDERS_ID        = "folder_id";
+    public static final String FOLDERS_LABEL     = "label";
     public static final String FOLDERS_TASKLISTS = "todolist";
     // - TaskLists
     public static final String TASKLISTS_KEY       = "todoLists";
-    public static final String TASKLISTS_ID        = "id";
+    public static final String TASKLISTS_ID        = "todo_list_id";
     public static final String TASKLISTS_TITLE     = "title";
     public static final String TASKLISTS_FOLDER_ID = "folder_id";
     // - Tasks
@@ -48,6 +48,9 @@ public final class RequestConstant {
     public static final String GOAL_TODO_UPDATED_DATE = "updated_at";
     public static final String GOAL_LABEL             = "label";
     public static final String GOAL_QUANTITY          = "quantity";
+    public static final String GOAL_INTERVAL_LABEL    = "intervalLabel";
+    public static final String GOAL_END_DATE          = "endDate";
+    public static final String GOAL_BEGIN_DATE        = "beginDate";
     // - GoalTodo
     public static final String GOAL_TODO_KEY           = "goalTodos";
     public static final String GOAL_TODO_ID            = "id";
@@ -55,9 +58,19 @@ public final class RequestConstant {
     public static final String GOAL_TODO_DONE_DATE     = "dateTimeDone";
     public static final String GOAL_TODO_DUE_DATE      = "dueDate";
 
+    // - Tags
+    public static final String TAG_KEY                 = "tags";
+    public static final String TAG_LABEL               = "label";
+
     // - Misc
     public static final String DONE                    = "done";
-    
+    public static final String ID                      = "id";
+
     // URL
     public final static String TASK_URL = BASE_URL + TODOS_EXTENSION;
+    public final static String FOLDER_URL = BASE_URL + FOLDERS_EXTENSION;
+    public final static String TODOLISTS_URL = BASE_URL + TODOLISTS_EXTENSION;
+    public final static String GOAL_URL = BASE_URL + GOALS_EXTENSION;
+    public final static String TAGS_URL = BASE_URL +TAGS_EXTENSION;
+
 }
