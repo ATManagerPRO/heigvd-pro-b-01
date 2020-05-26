632c4d09c8ec4291d8712e2c97bd579989922db7
teo <ferraricorneloup.teo@gmail.com>
2020-05-26 19:26:22 +0200

    it works

--- app/src/main/java/com/heig/atmanager/addContent/AddTaskFragment.java
+++ app/src/main/java/com/heig/atmanager/addContent/AddTaskFragment.java
@@ -267,7 +267,6 @@ public class AddTaskFragment extends Fragment {
                         MainActivity.getUser().addTask(newTask);
                         //update homeview
                         tasks = MainActivity.getUser().getHomeViewTasks();
-                        ((TaskFeedAdapter) tasksRecyclerView.getAdapter()).setTasks(tasks);
                     }
                 }
 
